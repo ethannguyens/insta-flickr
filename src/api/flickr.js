354650c876dcc6c9f9607e238ffcf36ffa4dd4d5
Ethan <ethan.nguyens@gmail.com>
2017-10-21 19:13:22 +0100

    flickr api to handle jsonp request

--- src/api/flickr.js
+++ src/api/flickr.js
@@ -1,12 +1,17 @@
-import Ajax from './ajax';
+const fetchJsonp = require('fetch-jsonp');
 
-const flickrUrl = "http://api.flickr.com/services/feeds/photos_public.gne?format=json&amp;tags=";
+const flickrUrl = "http://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=";
 
 class Flickr {
   static getFeeds(tags = 'nature') {
     return new Promise((resolve, reject) => {
-      Ajax(`${flickrUrl}${tags}`, json => {
-        resolve(json.items);
+      fetchJsonp(`${flickrUrl}${tags}`, {
+        jsonpCallbackFunction: 'jsonFlickrFeed'
+      }).then(response => {
+        response.json().then(json => resolve(json.items))
+      }).catch(function (err) {
+        console.log(err);
+        reject(err);
       });
     });
   }
