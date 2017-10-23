25c79c8480b041104a17bd5c7ef3b58e61f679c3
ethan.nguyen <ethan.nguyens@gmail.com>
2017-10-23 18:05:09 +0100

    change in search bar tests

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,48 @@
+import expect from 'expect';
+import React from 'react';
+import {mount} from 'enzyme';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import initialStates from '../../reducers/initialState';
+
+
+import Header from './Header';
+
+
+function setup() {
+  const props = {
+    menu: [{
+      id: "home",
+      link: "",
+      text: "home"
+    }]
+  };
+
+  const createStore = (state = initialStates) => {
+    const middleWares = [];
+    const mockStore = configureStore(middleWares);
+    return mockStore(state);
+  };
+  const store = createStore();
+
+  const wrapper = mount(<Provider store={store}><Header {...props}/></Provider>);
+  return {
+    props,
+    wrapper
+  }
+}
+
+describe('Header Component', () => {
+  it('should render itself', () => {
+    // Arrange & Act
+    const {wrapper} = setup();
+    // Assert
+    expect(wrapper.hasClass('header')).toBe(true);
+  });
+  it('should render SearchBar', () => {
+    // Arrange & Act
+    const {wrapper} = setup();
+    // Assert
+    expect(wrapper.find('SearchBar').hasClass('search-bar')).toBe(true);
+  });
+});
