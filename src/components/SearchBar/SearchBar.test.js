e084807139b12c291fd4b21c6352f3053c552f7f
ethan.nguyen <ethan.nguyens@gmail.com>
2017-10-23 17:50:43 +0100

    changes in search bar tests

---
+++ src/components/SearchBar/SearchBar.test.js
@@ -0,0 +1,33 @@
+import expect from 'expect';
+import React from 'react';
+import {mount} from 'enzyme';
+import SearchBar from './SearchBar';
+import initialStates from '../../reducers/initialState';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+
+function setup() {
+  const props = initialStates.instaFlickr;
+  const createStore = (state = initialStates) => {
+    const middleWares = [];
+    const mockStore = configureStore(middleWares);
+    return mockStore(state);
+  };
+  const store = createStore();
+
+  const wrapper = mount(<Provider store={store}><SearchBar/></Provider>);
+  return {
+    props,
+    wrapper
+  }
+}
+
+describe('SearchBar Component', () => {
+  it('should render itself', () => {
+    // Arrange & Act
+    const {wrapper} = setup();
+    // Assert
+    expect(wrapper.find('SearchBar').hasClass('search-bar')).toBe(true);
+  });
+});
